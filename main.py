c6386a466801e68a28261735d07c94c65e837037
Aleksei VIunik <vjunik88@gmail.com>
2024-10-27 01:58:26 +0200

    PEP8 fixes for the main.py file

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 """
 Главный файл. С него начинается работа программы. Он по сути делает только
-одно: запускает главный экран приложения. 
+одно: запускает главный экран приложения.
 """
+from gui.start_window import App
 
 import os
 import sys
 
 # Добавляем пути к папкам logic и gui
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'logic')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'gui')))
-
-# Импортируем приложение из gui
-from gui.start_window import App
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), 'logic'))
+    )
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), 'gui'))
+    )
 
 if __name__ == "__main__":
     import tkinter as tk
-    
+
     root = tk.Tk()
     app = App(root)
     root.mainloop()
