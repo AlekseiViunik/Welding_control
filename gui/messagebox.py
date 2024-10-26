2962a4de4fa0d04632544fb1357ca36230b30263
Aleksei VIunik <vjunik88@gmail.com>
2024-10-26 14:42:06 +0200

    use messagebox constant

--- gui/messagebox.py
+++ gui/messagebox.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
 
+from default_settings.gui_settings import ERROR_MESSAGE_TITLE
+
 def show_error(message: str):
     """Отображает всплывающее окно с сообщением об ошибке."""
     root = tk.Tk()
-    root.withdraw()  # Скрыть основное окно
-    messagebox.showerror("Ошибка", message)
+    root.withdraw()
+    messagebox.showerror(ERROR_MESSAGE_TITLE, message)
     root.destroy()
