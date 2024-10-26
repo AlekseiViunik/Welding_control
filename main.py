e62bcacfc84fbe6e500286afe245d65bfefe50e5
Aleksei VIunik <vjunik88@gmail.com>
2024-10-26 10:20:04 +0200

    add a start main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# Добавляем пути к папкам logic и gui
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'logic')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'gui')))
+
+# Импортируем приложение из gui
+from gui.start_window import App
+
+if __name__ == "__main__":
+    import tkinter as tk
+    
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
