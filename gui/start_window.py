94c645db76f8f195556ae2696cf86a05facab3c8
Aleksei VIunik <vjunik88@gmail.com>
2024-10-26 14:17:06 +0200

    Use new constants in the start_window file

--- gui/start_window.py
+++ gui/start_window.py
@@ -10,7 +10,9 @@ from default_settings.gui_settings import (
     TITLE, GEOMETRY, LABELS, LABEL_PADX, LABEL_ANCHOR, FRAME_FILL_AXIS,
     FRAME_PADX, ENTRY_WIDTH, ENTRY_EXPAND, ENTRY_FILL_AXIS, ENTRY_FRAME_SIDE,
     FRAME_BUTTON_TEXT, FRAME_BUTTON_LEFT_PADX, FRAME_BUTTON_RIGHT_PADX,
-    FRAME_BUTTON_SIDE, DIVIDER_HEIGHT, DIVIDER_FILL_AXIS
+    FRAME_BUTTON_SIDE, DIVIDER_HEIGHT, DIVIDER_FILL_AXIS, BUTTONS_WIDTH,
+    FRAME_BUTTONS_PADX, FRAME_BUTTONS_SIDE, BUTTONS_FRAME_PADY, BUTTON_TEXTS,
+    FIRST_ELEMENT, PATH_DIVIDER
     )
 from logic import get_xlsx
 
@@ -56,25 +58,26 @@ class App:
             # Разделительная строка
             tk.Frame(root, height=DIVIDER_HEIGHT).pack(fill=DIVIDER_FILL_AXIS)
 
-        # Кнопки "Погнали" и "Забить"
+        # Кнопки
         button_frame = tk.Frame(root)
-        button_frame.pack(pady=10)
+        button_frame.pack(pady=BUTTONS_FRAME_PADY)
 
-        start_button = tk.Button(button_frame, text="Погнали", command=self.start_process)
-        start_button.pack(side='left', padx=5)
-
-        clear_button = tk.Button(button_frame, text="Забить", command=self.clear_entries)
-        clear_button.pack(side='left', padx=5)
-
-        settings_button = tk.Button(button_frame, text="Настройки", command=None)
-        settings_button.pack(side='left', padx=5)
+        for text in BUTTON_TEXTS.keys():
+            command = getattr(self, BUTTON_TEXTS[text])
+            button = tk.Button(
+                button_frame,
+                text=text,
+                command=command,
+                width=BUTTONS_WIDTH
+                )
+            button.pack(side=FRAME_BUTTONS_SIDE, padx=FRAME_BUTTONS_PADX)
 
     def browse_file(self, entry):
         """Открывает диалог выбора файлов."""
         file_paths = filedialog.askopenfilenames()  # Позволяем выбирать несколько файлов
         if file_paths:
-            entry.delete(0, tk.END)  # Очищаем текущее значение
-            entry.insert(0, '; '.join(file_paths))  # Вставляем выбранные пути через точку с запятой
+            entry.delete(FIRST_ELEMENT, tk.END)  # Очищаем текущее значение
+            entry.insert(FIRST_ELEMENT, PATH_DIVIDER.join(file_paths))  # Вставляем выбранные пути через точку с запятой
 
     def start_process(self):
         """Запускает процесс обработки."""
@@ -92,7 +95,12 @@ class App:
     def clear_entries(self):
         """Очищает все текстовые поля."""
         for entry in self.file_paths:
-            entry.delete(0, tk.END)
+            entry.delete(FIRST_ELEMENT, tk.END)
+    
+    def open_settings(self):
+        """Открывает окно настроек."""
+        # TODO реализовать метод
+        pass
 
 if __name__ == "__main__":
     root = tk.Tk()
