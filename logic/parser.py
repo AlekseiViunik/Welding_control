344eaa6634bb9512e57775f75f37ff92f60815b7
Aleksei VIunik <vjunik88@gmail.com>
2024-10-27 17:59:00 +0100

    rename default_settings folder and add pep8 fixes for the parser file

--- logic/parser.py
+++ logic/parser.py
@@ -6,9 +6,9 @@
 1. В parse_weld_data передаем массив путей с файлами одного типа контроля.
    Файлов может быть несколько. Пробегаемся циклом по каждому файлу
 2. Открываем файл и пробегаемся циклом по каждой странице файла.
-3. На каждой странице пробегаемя циклом по каждой строке и проверяем ее на 
+3. На каждой странице пробегаемя циклом по каждой строке и проверяем ее на
    соответствие регулярному выражению, записанному в weld_number_pattern.
-   Регулярки соответствуют все строки типа <Одна_из_CYTNHСНТУ_букв> + 
+   Регулярки соответствуют все строки типа <Одна_из_CYTNHСНТУ_букв> +
    любое количество цифр и дефисов в любой последовательности.
    ВАЖНО: номер шва должен быть в столбце A. Столбцы могут быть объединены,
    но в объединенных столбцах тогда должен быть столбец A.
@@ -19,19 +19,20 @@
    для этого номера шва. Если нет, создаем его. Для каждого номера шва
    в словаре значением будет еще один словарь типа {тип_контроля: дата}.
    Если ключ с таким номером шва есть - добавляем в его значения тип контроля
-   (передан как key в метод) и его дату. 
+   (передан как key в метод) и его дату.
 """
 import re
 import openpyxl
 from datetime import datetime
 
-from default_settings.logic_settings import (
+from settings.logic_settings import (
     WELD_ID_REGEXP, REPLACEMENT_DICT, DATE_REGEXP, DATE_FORMAT
 )
 
 # TODO подключить Redis(?) для хранения данных вместо словаря
 welds_data = {}
 weld_number_pattern = re.compile(WELD_ID_REGEXP)
+
 
 def parse_weld_data(paths, key):
     """Парсим все страницы переданных файлов в поисках номеров швов и дат
@@ -44,8 +45,10 @@ def parse_weld_data(paths, key):
         for sheet in wb.worksheets:
             for row in sheet.iter_rows(min_row=1):
                 weld_number = str(row[0].value)
-                if (weld_number and 
-                    weld_number_pattern.match(weld_number)):
+                if (
+                    weld_number and
+                    weld_number_pattern.match(weld_number)
+                ):
                     date_found = False
                     weld_number_ru = (
                         REPLACEMENT_DICT[weld_number[0]] + weld_number[1:]
@@ -57,14 +60,18 @@ def parse_weld_data(paths, key):
                     for cell in row[1:]:
                         cell_value = cell.value
                         if isinstance(cell_value, datetime):
-                            welds_data[weld_number_ru][key] = cell_value.strftime(DATE_FORMAT)
+                            welds_data[weld_number_ru][key] = (
+                                cell_value.strftime(DATE_FORMAT)
+                            )
                             date_found = True
                             break
-                        elif (isinstance(cell_value, str) and 
-                              re.search(
+                        elif (
+                            isinstance(cell_value, str) and
+                            re.search(
                                   DATE_REGEXP,
                                   cell_value
-                              )):
+                            )
+                        ):
                             try:
                                 # Пытаемся разобрать строку как дату
                                 date_found = True
