36c66cf9ad58c30b226fce61b241ea0f3a0c6cf6
Aleksei VIunik <vjunik88@gmail.com>
2024-10-27 18:46:43 +0100

    add default path to save summary file

---
+++ settings/user_settings.py
@@ -0,0 +1,14 @@
+import os
+
+# Указываем путь сохранения итогового файла по умолчанию на рабочий стол
+# Default:
+# os.path.join(
+#    os.path.expanduser("~"),
+#    "Desktop",
+#    "summary.xlsx"
+# )
+DEFAULT_SAVE_PATH = os.path.join(
+    os.path.expanduser("~"),  # Домашняя директория
+    "Desktop",  # Рабочий стол
+    "summary.xlsx" # Имя файла
+)
