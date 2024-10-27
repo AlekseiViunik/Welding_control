9f58dc086d629f1886a72446170de6759f1f68a5
Aleksei VIunik <vjunik88@gmail.com>
2024-10-27 02:05:17 +0200

    PEP8 fixes for the messagebox.py file

--- gui/messagebox.py
+++ gui/messagebox.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 
 from default_settings.gui_settings import ERROR_MESSAGE_TITLE
 
+
 def show_error(message: str):
     """Отображает всплывающее окно с сообщением об ошибке."""
     root = tk.Tk()
