19faf26cec14bdb1fce84ffa43e95ae89a9fe5b2
Aleksei VIunik <vjunik88@gmail.com>
2024-10-29 11:54:06 +0100

    set DEFAULT_SAVE_PATH as oneline setting

--- settings/user_settings.py
+++ settings/user_settings.py
@@ -3,12 +3,8 @@ import os
 # Указываем путь сохранения итогового файла по умолчанию на рабочий стол
 # Default:
 # os.path.join(
-#    os.path.expanduser("~"),
-#    "Desktop",
-#    "summary.xlsx"
+#    os.path.expanduser("~"),  # Домашняя директория
+#    "Desktop",  # Рабочий стол
+#    "summary.xlsx"  # Имя файла
 # )
-DEFAULT_SAVE_PATH = os.path.join(
-    os.path.expanduser("~"),  # Домашняя директория
-    "Desktop",  # Рабочий стол
-    "summary.xlsx"  # Имя файла
-)
+DEFAULT_SAVE_PATH = os.path.join(os.path.expanduser("~"),"Desktop","summary.xlsx")
