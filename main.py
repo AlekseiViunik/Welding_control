4d1abbe24ff5c37a763ef3cb74a609a1619de212
Aleksei VIunik <vjunik88@gmail.com>
2024-10-30 20:26:07 +0100

    remove unnecessary comments

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import sys
 from settings import user_settings as us
 
 
-# Устанавливаем путь сохранения итогового файла по умолчанию
 default_settings = {
     us.SAVE_PATH_KEY: us.DEFAULT_SAVE_PATH
 }
@@ -20,11 +19,9 @@ if not os.path.exists(us.SETTINGS_FILE_NAME):
     with open(us.SETTINGS_FILE_NAME, 'w') as f:
         json.dump(default_settings, f)
 else:
-    # Если файл существует, проверяем значение DEFAULT_SAVE_PATH
     with open(us.SETTINGS_FILE_NAME, 'r') as f:
         settings = json.load(f)
 
-    # Проверяем, пустое ли значение для ключа DEFAULT_SAVE_PATH
     if not settings.get(us.SAVE_PATH_KEY):
         summary_file_path = (
             default_settings[us.SAVE_PATH_KEY] +
@@ -32,11 +29,9 @@ else:
             us.SAVE_FILE_NAME
         )
         settings[us.SAVE_PATH_KEY] = summary_file_path
-        # Записываем обновленные настройки обратно в файл
         with open(us.SETTINGS_FILE_NAME, 'w') as f:
             json.dump(settings, f, indent=us.JSON_INDENT)
 
-# Добавляем пути к папкам logic и gui
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), us.LOGIC_PATH))
 )
