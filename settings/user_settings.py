3d55d1b8826ea06778b7793585dfea1fe2f8844b
Aleksei VIunik <vjunik88@gmail.com>
2024-10-30 11:36:55 +0100

    breake pep8 rules for user_settings

--- settings/user_settings.py
+++ settings/user_settings.py
@@ -2,6 +2,5 @@ import os
 
 # Указываем путь сохранения итогового файла по умолчанию на рабочий стол
 # Default:
-# os.path.join(
-#    os.path.expanduser("~"), "Desktop", "summary.xlsx")
+# os.path.join(os.path.expanduser("~"), "Desktop", "summary.xlsx")
 DEFAULT_SAVE_PATH = os.path.join(os.path.expanduser("~"), "Desktop", "summary.xlsx")
