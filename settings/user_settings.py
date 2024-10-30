7092919bfe97b8eff1684f013348c2b09f1a4da9
Aleksei VIunik <vjunik88@gmail.com>
2024-10-30 15:08:37 +0100

    add some new settings to user_settings

--- settings/user_settings.py
+++ settings/user_settings.py
@@ -16,3 +16,16 @@ DEFAULT_SAVE_PATH = os.path.join(
 # Указываем имя файла, под которым будут сохраняться пользовательские
 # настройки
 SETTINGS_FILE_NAME = "settings.json"  # Default: "settings.json"
+
+# Здесь указан ключ файла json, значением которого является путь к сохранению
+# итогового файла
+SAVE_PATH_KEY = "DEFAULT_SAVE_PATH"  # Default: "DEFAULT_SAVE_PATH"
+
+# Путь из корня проекта к папке с логикой
+LOGIC_PATH = 'logic'  # Default: 'logic'
+
+# Путь из корня проекта к папке с интерфейсом
+GUI_PATH = 'gui'  # Default: 'gui'
+
+# Отступ в пробелах для записи в json файл
+JSON_INDENT = 4  # Default: 4
