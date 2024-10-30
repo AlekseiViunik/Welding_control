72473872a20c4c2a6f24766b9e0bda3fa52d0ba0
Aleksei VIunik <vjunik88@gmail.com>
2024-10-30 19:47:13 +0100

    use const instead of variable. send path when creating an App object

--- main.py
+++ main.py
@@ -10,26 +10,30 @@ import sys
 
 from settings import user_settings as us
 
-settings_file = us.SETTINGS_FILE_NAME
 
 # Устанавливаем путь сохранения итогового файла по умолчанию
 default_settings = {
     us.SAVE_PATH_KEY: us.DEFAULT_SAVE_PATH
 }
 
-if not os.path.exists(settings_file):
-    with open(settings_file, 'w') as f:
+if not os.path.exists(us.SETTINGS_FILE_NAME):
+    with open(us.SETTINGS_FILE_NAME, 'w') as f:
         json.dump(default_settings, f)
 else:
     # Если файл существует, проверяем значение DEFAULT_SAVE_PATH
-    with open(settings_file, 'r') as f:
+    with open(us.SETTINGS_FILE_NAME, 'r') as f:
         settings = json.load(f)
 
     # Проверяем, пустое ли значение для ключа DEFAULT_SAVE_PATH
     if not settings.get(us.SAVE_PATH_KEY):
-        settings[us.SAVE_PATH_KEY] = default_settings[us.SAVE_PATH_KEY]
+        summary_file_path = (
+            default_settings[us.SAVE_PATH_KEY] +
+            "/" +
+            us.SAVE_FILE_NAME
+        )
+        settings[us.SAVE_PATH_KEY] = summary_file_path
         # Записываем обновленные настройки обратно в файл
-        with open(settings_file, 'w') as f:
+        with open(us.SETTINGS_FILE_NAME, 'w') as f:
             json.dump(settings, f, indent=us.JSON_INDENT)
 
 # Добавляем пути к папкам logic и gui
@@ -43,6 +47,9 @@ sys.path.append(
 if __name__ == "__main__":
     import tkinter as tk
 
+    with open(us.SETTINGS_FILE_NAME, 'r') as f:
+        settings = json.load(f)
+        save_path = settings[us.SAVE_PATH_KEY]
     root = tk.Tk()
-    app = App(root)
+    app = App(root, save_path)
     root.mainloop()
