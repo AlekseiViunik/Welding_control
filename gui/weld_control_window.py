4705e8c1eca51719f7358e19c252bf52a743c43c
Aleksei VIunik <vjunik88@gmail.com>
2024-11-01 22:23:40 +0100

    rename start_window. Set it as weld_control_window. Delegate some methods to the new start_window

--- gui/weld_control_window.py
+++ gui/weld_control_window.py
@@ -2,49 +2,43 @@
 GUI гллавного экрана. На нем мы выбираем файлы для проверки и рисуем кнопки
 Также тут описаны главные параметры экрана и действия нажатия кнопок.
 """
-import ctypes
 import json
 import logging
-import os
-import sys
 import tkinter as tk
-from threading import Thread
-from tkinter import PhotoImage
 
 from gui.settings_window import SettingsWindow
+from gui.app_helper import AppHelper
 from logic.get_xlsx import GetXlsx
 from settings import gui_settings as gs
 from settings import logging_settings as log
 from settings import user_settings as us
 
-from .app_helper import AppHelper
-
 
 class WeldControlWindow:
-    def __init__(self, root, save_path):
-        self.root = root
-        self.save_path = save_path
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.helper = AppHelper(root)
-        self.settings = SettingsWindow(root)
-        self.root.title(gs.MAIN_WINDOW_TITLE)
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.helper = AppHelper(self.root)
+        self.settings = SettingsWindow(self.root)
+        self.root.title(gs.WELD_CONTROL_WINDOW_TITLE)
         self.root.geometry(f"{gs.WINDOW_WIDTH}x{gs.WINDOW_HEIGHT}")
-        self.set_window_icon()
         self.helper.center_window(
             self.root,
             gs.WINDOW_WIDTH,
             gs.WINDOW_HEIGHT
         )
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.file_paths = []
         self.threads = []
         self.stop_threads = False
+        self.save_path = self.get_save_path()
 
         for label_text in gs.LABELS:
-            entry = self.helper.create_label_entry_frame(root, label_text)
+            entry = self.helper.create_label_entry_frame(self.root, label_text)
             self.file_paths.append(entry)
 
         # Кнопки
-        button_frame = tk.Frame(root)
+        button_frame = tk.Frame(self.root)
         button_frame.pack(pady=gs.BUTTONS_FRAME_PADY)
 
         for text, command_name in gs.BUTTON_TEXTS.items():
@@ -57,15 +51,15 @@ class WeldControlWindow:
                 )
             button.pack(side=gs.FRAME_BUTTONS_SIDE, padx=gs.FRAME_BUTTONS_PADX)
 
-        label = tk.Label(root, text=gs.AUTHOR_LABEL_TEXT)
-        label.place(
-            relx=gs.AUTHOR_RELX,
-            rely=gs.AUTHOR_RELY,
-            anchor=gs.AUTHOR_ANCHOR,
-            y=-gs.AUTHOR_PADY
-        )
+            label = tk.Label(self.root, text=gs.AUTHOR_LABEL_TEXT)
+            label.place(
+                relx=gs.AUTHOR_RELX,
+                rely=gs.AUTHOR_RELY,
+                anchor=gs.AUTHOR_ANCHOR,
+                y=-gs.AUTHOR_PADY
+            )
 
-    def start_process(self):
+    def start(self):
         """Запускает процесс обработки и отображает информационное окно."""
         logging.info(log.LOG_DIVIDER)
         logging.info(log.LOG_START)
@@ -77,10 +71,7 @@ class WeldControlWindow:
         logging.info(f"Путь для сохранения итогового файла: {self.save_path}")
         self.helper.show_info_window()
         logging.info(log.LOG_CALCULATE_DATES_CALL)
-        thread = Thread(target=self.calculate_dates)
-        thread.daemon = True
-        thread.start()
-        self.threads.append(thread)
+        self.root.after(0, self.calculate_dates)
 
     def calculate_dates(self):
         """Запускает логику обработки процесса."""
@@ -128,36 +119,8 @@ class WeldControlWindow:
         self.settings.create_window()
         self.get_save_path()
 
-    def set_window_icon(self):
-        """Устанавливает иконку для окна."""
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
-            'Weld Control'
-        )
-
-        if getattr(sys, 'frozen', False):
-            # Если запущено как исполняемое приложение
-            icon_path_png = os.path.join(
-                sys._MEIPASS,
-                gs.ICONS_FOLDER_NAME,
-                gs.PNG_ICON_FILENAME
-            )
-            icon_path_ico = os.path.join(
-                sys._MEIPASS,
-                gs.ICONS_FOLDER_NAME,
-                gs.ICO_ICON_FILENAME
-            )
-        else:
-            # Если запущено из исходников
-            icon_path_png = gs.PNG_ICON_FILEPATH
-            icon_path_ico = gs.ICO_ICON_FILEPATH
-
-        self.icon = PhotoImage(file=icon_path_png)
-        self.root.iconphoto(True, self.icon)
-        self.root.iconbitmap(icon_path_ico)
-
     def on_closing(self):
         """Обработчик закрытия окна."""
         self.stop_threads = True
         if self.helper.info_window:
             self.helper.info_window.destroy()
-        self.root.destroy()
