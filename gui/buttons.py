06ce15552489353389f7768dd6b2c26eb716babb
Aleksei VIunik <vjunik88@gmail.com>
2024-11-04 20:58:54 +0100

    update buttons class

--- gui/buttons.py
+++ gui/buttons.py
@@ -8,16 +8,14 @@ from settings.gui.components import (
 
 class Buttons:
 
-    def __init__(self, root, instance, parent=None):
+    def __init__(self, root, instance):
         self.root = root
         self.instance = instance
-        self.parent = parent
 
     def create_buttons_frame(self, buttons_name_and_process):
         button_frame = tk.Frame(self.root)
         button_frame.pack(pady=fr.BUTTONS_FRAME_PADY)
         for name, command_name in buttons_name_and_process.items():
-            args = (self.instance.current_path, self.parent)
             command = getattr(self.instance, command_name)
             self.create_button(
                 button_frame,
@@ -27,7 +25,6 @@ class Buttons:
                 side=btn.BUTTONS_SIDE,
                 left_padx=btn.BUTTONS_PADX,
                 right_padx=btn.BUTTONS_PADX,
-                args=args
             )
 
     def create_button(
@@ -40,12 +37,11 @@ class Buttons:
             left_padx=0,
             right_padx=0,
             pady=0,
-            args=None
     ):
         button = tk.Button(
             window,
             text=button_name,
-            command=lambda: process(*args) if args else process(),
+            command=process,
             width=width
         )
         button.pack(side=side, padx=(left_padx, right_padx), pady=pady)
