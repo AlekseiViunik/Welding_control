63122509df9ad2515d6255856455bf7c6595fa3f
Aleksei VIunik <vjunik88@gmail.com>
2024-11-05 09:04:14 +0100

    add create_browse_frame method to Buttons

--- gui/buttons.py
+++ gui/buttons.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 
+from gui.app_helper import AppHelper
 from settings.gui.components import (
     frames as fr,
-    buttons as btn
+    buttons as btn,
+    labels as lbl,
+    textfields as txt
 )
 
 
@@ -11,6 +14,46 @@ class Buttons:
     def __init__(self, root, instance):
         self.root = root
         self.instance = instance
+        self.helper = AppHelper(instance)
+
+    def create_browse_frame(self, label_text, hint='', choose_directory=False):
+        browse_frame = tk.Frame(self)
+        browse_frame.pack(
+            fill=fr.BROWSE_FRAME_FILL_AXIS,
+            padx=fr.BROWSE_FRAME_PADX
+        )
+        label = tk.Label(browse_frame, text=label_text)
+        label.pack(anchor=lbl.LABEL_ANCHOR)
+        entry = tk.Entry(
+            browse_frame,
+            textvariable=hint,
+            width=txt.TEXT_FIELD_WIDTH
+        )
+        entry.pack(
+            side=txt.TEXT_FIELD_FRAME_SIDE,
+            fill=txt.TEXT_FIELD_FILL_AXIS,
+            expand=txt.TEXT_FIELD_EXPAND
+        )
+        button = tk.Button(
+            browse_frame,
+            text=btn.BROWSE_BUTTON_TEXT,
+            command=lambda e=entry: self.helper.browse_file(
+                e,
+                choose_directory,
+                self.instance
+            )
+        )
+        button.pack(
+            side=btn.BROWSE_BUTTON_SIDE,
+            padx=(btn.BROWSE_BUTTON_LEFT_PADX, btn.BROWSE_BUTTON_RIGHT_PADX)
+        )
+
+        tk.Frame(
+            self.instance,
+            height=fr.DIVIDER_HEIGHT
+        ).pack(fill=fr.DIVIDER_FILL_AXIS)
+
+        return entry
 
     def create_buttons_frame(self, buttons_name_and_process):
         button_frame = tk.Frame(self.root)
