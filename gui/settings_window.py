0047e08dd35d1c13cf31c083f4d5f21c50db9763
Aleksei VIunik <vjunik88@gmail.com>
2024-11-06 21:43:30 +0100

    Use the Frame class to create save and cancel buttons

--- gui/settings_window.py
+++ gui/settings_window.py
@@ -8,7 +8,6 @@ from settings import user_settings as us
 from settings.gui.components import (
     buttons as btn,
     labels as lbl,
-    frames as fr
 )
 from settings.gui.windows import (
     windows as win,
@@ -30,54 +29,28 @@ class SettingsWindow:
         settings = self.load_settings(us.SETTINGS_FILE_NAME)
         save_path = settings[us.SAVE_PATH_KEY]
 
-        # Текстовое поле с текущим путем сохранения
-        # Устанавливаем текущее значение по умолчанию
-        frame = Frame(settings_window)
+        frame = Frame(settings_window, self)
         current_path = tk.StringVar(
             value=save_path
         )
+
+        # Фрейм с лейблом, текстовым полем для ввода и кнопкой "Обзор"
         frame.create_entry_frame(lbl.WHERE_TO_SAVE, current_path, True)
 
-        # self.helper.create_label_entry_frame(
-        #     settings_window,
-        #     lbl.WHERE_TO_SAVE,
-        #     current_path,
-        #     True
-        # )
-
-        # Кнопки Сохранить и Отмена
-        button_frame = tk.Frame(settings_window)
-        button_frame.pack(pady=fr.SETTINGS_BUTTONS_FRAME_PADY)
-
-        # Кнопка Сохранить
-        save_button = tk.Button(
-            button_frame,
-            text=btn.SAVE_BUTTON_NAME,
-            command=lambda: self.save_settings(
-                current_path.get(),
-                settings_window
-            )
-        )
-        save_button.pack(
-            side=btn.SETTINGS_BUTTONS_PACK_SIDE,
-            padx=btn.SETTINGS_BUTTONS_PADX
-        )
-
-        # Кнопка Отмена
-        cancel_button = tk.Button(
-            button_frame,
-            text=btn.CANCEL_BUTTON_NAME,
-            command=settings_window.destroy
-        )
-        cancel_button.pack(
-            side=btn.SETTINGS_BUTTONS_PACK_SIDE,
-            padx=btn.SETTINGS_BUTTONS_PADX
+        # Фрейм с кнопками "Сохранить" и "Отмена"
+        buttons_args = {
+            btn.SAVE_BUTTON_NAME: [current_path, settings_window],
+            btn.CANCEL_BUTTON_NAME: [settings_window]
+        }
+        frame.create_button_frame(
+            btn.settings_buttons_name_to_process,
+            buttons_args
         )
 
     def save_settings(self, path, window):
         """Сохраняет настройки в JSON файл."""
         settings = self.load_settings(us.SETTINGS_FILE_NAME)
-        settings[us.SAVE_PATH_KEY] = path
+        settings[us.SAVE_PATH_KEY] = path.get()
 
         with open(us.SETTINGS_FILE_NAME, 'w') as f:
             json.dump(settings, f, indent=us.JSON_INDENT)
@@ -86,8 +59,11 @@ class SettingsWindow:
             info.SUCCESS_TITLE,
             info.SAVED_SETTINGS_SUCCESS_MESSAGE,
         )
-        window.destroy()  # Закрываем окно настроек после сохранения
+        window.destroy()
 
     def load_settings(self, settings_file):
         with open(settings_file, 'r') as f:
             return json.load(f)
+
+    def destroy(self, instance):
+        instance.destroy()
