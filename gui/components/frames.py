3dce2c0c25936e73e37b95b904314c5bc7b4719c
Aleksei VIunik <vjunik88@gmail.com>
2024-11-06 21:42:20 +0100

    add args to the button commands in the Frame class

--- gui/components/frames.py
+++ gui/components/frames.py
@@ -11,8 +11,9 @@ from settings.gui.windows import windows as win
 
 
 class Frame:
-    def __init__(self, root):
+    def __init__(self, root, instance):
         self.root = root
+        self.instance = instance
 
     def create_entry_frame(self, label_text, hint='', choose_directory=False):
         """Создает фрейм с лейблом и текстовым полем."""
@@ -39,19 +40,24 @@ class Frame:
 
         return entry
 
-    def create_button_frame(self, buttons_name_and_process):
+    def create_button_frame(self, buttons_name_and_process, buttons_args=None):
         """Создает фрейм с кнопками."""
+        if buttons_args is None:
+            buttons_args = {}
+
         button_frame = tk.Frame(self.root)
-        button_frame.pack(pady=btn.BUTTONS_FRAME_PADY)
+        button_frame.pack(pady=btn.FRAME_BUTTONS_PADY)
 
         for name, command_name in buttons_name_and_process.items():
+            command = getattr(self.instance, command_name)
+            command_args = buttons_args.get(name, [])
             button = tk.Button(
                 button_frame,
                 text=name,
-                command=getattr(self.root, command_name),
+                command=lambda cmd=command, args=command_args: cmd(*args),
                 width=btn.BUTTONS_WIDTH
             )
-            button.pack(side=tk.LEFT, padx=btn.BUTTONS_PADX)
+            button.pack(side=tk.LEFT, padx=btn.FRAME_BUTTONS_PADX)
 
     def browse_file(self, entry, choose_directory=False, parent=None):
         """Открывает диалог выбора файла или директории."""
