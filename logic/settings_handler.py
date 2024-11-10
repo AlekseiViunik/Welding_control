777ee608d77a8e59b5d8b9e3515439a0c25ecab9
Aleksei VIunik <vjunik88@gmail.com>
2024-11-10 22:00:23 +0100

    create settings_handler

---
+++ logic/settings_handler.py
@@ -0,0 +1,18 @@
+import json
+
+from settings import settings as set
+
+
+class SettingsHandler:
+    def __init__(self):
+        self.settings_filename = set.SETTINGS_FILE_NAME
+
+    def file_read(self, to_return='', endcoding=set.ENCODING):
+        with open(self.settings_filename, 'r', encoding=endcoding) as f:
+            settings = json.load(f)
+            if to_return:
+                return settings[to_return]
+            return settings
+
+    def file_write(self, to_write=None, encoding=set.ENCODING):
+        pass
